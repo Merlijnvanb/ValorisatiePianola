80586efc06ceb175955b5df0264cb97a8d6bf582
Merlijnvanb <merlijnvanballegooijen@outlook.com>
2026-09-18 00:32:40 +0200

    meer camera werk

--- PianolaUnity/Assets/Scripts/CameraController.cs
+++ PianolaUnity/Assets/Scripts/CameraController.cs
@@ -1,33 +1,23 @@
 using UnityEngine;
 using Unity.Mathematics;
 using System.Collections;
+using PrimeTween;
 
 public class CameraController : MonoBehaviour
 {
-    [System.Serializable]
-    public struct Neighbor
-    {
-        public CameraPoint point;
-        public int2 vector;
-    }
-    
-    [System.Serializable]
-    public struct Point
-    {
-        public CameraPoint mainPoint;
-        public Neighbor[] neighbors;
-    }
+    public float TweenDuration = .75f;
+    public CameraPoint[] Points;
 
-    public float LerpSpeed = .75f;
-    public Point[] Points;
-
-    private Point currentPoint;
+    private CameraPoint currentPoint;
+    private Sequence currentTweens;
     private Camera mainCam;
 
     void Start()
     {
         mainCam = Camera.main;
-        ChangePoint(Points[0]);
+        
+        currentPoint = Points[0];
+        mainCam.transform.SetPositionAndRotation(Points[0].transform.position, Points[0].transform.rotation);
     }
 
     void Update()
@@ -36,32 +26,46 @@ public class CameraController : MonoBehaviour
             return;
     }
 
-    private void ChangePoint(Point point)
+    private bool ChangePoint(CameraPoint point)
     {
-        StartCoroutine(LerpTransform(mainCam.transform, point.mainPoint.transform));
+        if (currentTweens.isAlive)
+            return false;
+        
+        var startPos = mainCam.transform.position;
+        var endPos = point.transform.position;
+        
+        var startRot = mainCam.transform.rotation;
+        var endRot = Quaternion.LookRotation(point.GetViewVector());
+
+        currentTweens = Sequence.Create()
+            .Group(Tween.Position(mainCam.transform, startPos, endPos, TweenDuration, Ease.InOutCubic))
+            .Group(Tween.Rotation(mainCam.transform, startRot, endRot, TweenDuration, Ease.InOutCubic));
+        
         currentPoint = point;
+
+        return true;
     }
     
-    private IEnumerator LerpTransform(Transform oldT, Transform newT)
-    {
-        var t = 0f;
-
-        var oldPos = oldT.transform.position;
-        var oldRot = oldT.transform.rotation;
-
-        var newPos = newT.transform.position;
-        var newRot = newT.transform.rotation;
-
-        while (t < 1f)
-        {
-            mainCam.transform.position = Vector3.Slerp(oldPos, newPos, t);
-            mainCam.transform.rotation = Quaternion.Slerp(oldRot, newRot, t);
-
-            t += LerpSpeed * Time.deltaTime;
-            yield return null;
-        }
-        
-        mainCam.transform.position = newPos;
-        mainCam.transform.rotation = newRot;
-    }
+    // private IEnumerator LerpTransform(Transform oldT, Transform newT)
+    // {
+    //     var t = 0f;
+    //
+    //     var oldPos = oldT.transform.position;
+    //     var oldRot = oldT.transform.rotation;
+    //
+    //     var newPos = newT.transform.position;
+    //     var newRot = newT.transform.rotation;
+    //
+    //     while (t < 1f)
+    //     {
+    //         mainCam.transform.position = Vector3.Lerp(oldPos, newPos, t);
+    //         mainCam.transform.rotation = Quaternion.Lerp(oldRot, newRot, t);
+    //
+    //         t += LerpSpeed * Time.deltaTime;
+    //         yield return null;
+    //     }
+    //     
+    //     mainCam.transform.position = newPos;
+    //     mainCam.transform.rotation = newRot;
+    // }
 }
--- PianolaUnity/Assets/Scripts/CameraPoint.cs
+++ PianolaUnity/Assets/Scripts/CameraPoint.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
+using Unity.Mathematics;
 
 public class CameraPoint : MonoBehaviour
 {
-    public Transform target;
+    [System.Serializable]
+    public struct Neighbor
+    {
+        public CameraPoint point;
+        public int2 vector;
+    }
+    
+    public Transform Target;
+    public Neighbor[] Neighbors;
+
+    public Vector3 GetViewVector()
+    {
+        return Target.position - transform.position;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawLine(transform.position, Target.position);
+        Gizmos.DrawSphere(Target.position, 0.5f);
+    }
 }
